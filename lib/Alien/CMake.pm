5451bd39c2d0872415b91a817adeb1c183e82123
Tobias Leich <email@froggs.de>
2014-02-02 11:42:31 +0100

    bump version to 0.05

--- lib/Alien/CMake.pm
+++ lib/Alien/CMake.pm
@@ -13,11 +13,11 @@ Alien::CMake - Build and make available CMake library - L<http://cmake.org/>
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 $VERSION = eval $VERSION;
 
 =head1 SYNOPSIS
