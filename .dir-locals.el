5072dea1987b346330c86f8f4ce362c6969bab37
Andrew Kirkpatrick <ubermonk@gmail.com>
2015-02-03 08:49:35 +1030

    disable tabs in .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,7 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((cperl-mode
+((nil
+  (indent-tabs-mode . nil))
+ (cperl-mode
   (cperl-indent-level . 2)))
