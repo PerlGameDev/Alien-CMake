9881391955fa0a47b2b4a345532e5c5d3f505a44
Graham Ollis <plicease@cpan.org>
2016-02-13 08:37:25 -0500

    version 0.07

--- lib/Alien/CMake.pm
+++ lib/Alien/CMake.pm
@@ -13,11 +13,11 @@ Alien::CMake - Build and make available CMake library - L<http://cmake.org/>
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =cut
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 $VERSION = eval $VERSION;
 
 =head1 SYNOPSIS
